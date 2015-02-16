eea488ced87130e88b77518f6a8a07401b48c735
Ros360 <ryanros360@aol.com>
2015-02-15 22:25:54 -0600

    Added pause feature

---
+++ Assets/Pause.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour 
+{
+	public bool canPause;
+	// Use this for initialization
+	void Start () 
+	{
+		canPause = true;
+	}
+
+
+	void OnGUI () 
+	{
+		if (GUI.Button (new Rect (630, 15, 100, 50), "Pause Button")) 
+		{
+			if (canPause)
+			{
+				Time.timeScale = 0;
+				canPause = false;
+			}
+			else
+			{
+				Time.timeScale = 1;
+				canPause = true;
+			}
+		}
+	}
+}
